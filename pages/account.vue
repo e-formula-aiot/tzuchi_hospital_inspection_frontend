d80364ff44010a49ee356ad8c55248070f96c8ba
Chris_lu <66943605+Chris840307@users.noreply.github.com>
2021-03-05 11:56:52 +0800

    新增人員管理

---
+++ pages/account.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title>
+        <span>人員管理</span>
+      </v-card-title>
+      <v-card-text>
+        <v-data-table
+          fixed-header
+          dense
+          :headers="headers"
+          :items="dataTable"
+          item-key="_id"
+          :search="search"
+          :items-per-page="25"
+          :footer-props="{
+            'items-per-page-options': [25, 50, 100],
+          }"
+          class="elevation-1"
+        />
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import backendService from '@/services/backendService';
+
+export default {
+  name: 'account',
+  data() {
+    return {
+      search: '',
+      headers: [
+        { text: '工號', value: 'employee_id' },
+        { text: '姓名', value: 'user_name' },
+        { text: '稱謂', value: 'user_title' },
+        { text: 'Email', value: 'email' },
+        { text: '紀錄人員所屬部門', value: 'deparment_id' },
+        { text: '紀錄 Line ID', value: 'line_user_id' },
+      ],
+      dataTable: [],
+    };
+  },
+  mounted() {
+    this.getUser();
+  },
+  created() {},
+  components: {},
+  methods: {
+    getUser() {
+      backendService
+        .getUser()
+        .then((response) => {
+          this.dataTable = response.data;
+        })
+        .catch((error) => {
+          console.log(error.response.data.msg);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
--- services/backendService.js
+++ services/backendService.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 
 const backendService = {
+  getUser: () => {
+    const config = {
+      method: 'GET',
+      url: `${process.env.backendUrl}user/`,
+      dataType: 'json',
+      contentType: 'application/json',
+    };
+    return axios(config);
+  },
   getCategory: () => {
     const config = {
       method: 'GET',
@@ -28,42 +37,15 @@ const backendService = {
     };
     return axios(config);
   },
-  putHistory: () => {
-    const config = {
-      method: 'PUT',
-      url: `${process.env.backendUrl}history.php`,
-      dataType: 'json',
-      contentType: 'application/json',
-    };
-    return axios(config);
-  },
-  getGift: () => {
-    const config = {
-      method: 'GET',
-      url: `${process.env.backendUrl}gift.php`,
-      dataType: 'json',
-      contentType: 'application/json',
-    };
-    return axios(config);
-  },
-  getGiftById: (id) => {
-    const config = {
-      method: 'GET',
-      url: `${process.env.backendUrl}gift.php?id=${id}`,
-      dataType: 'json',
-      contentType: 'application/json',
-    };
-    return axios(config);
-  },
-  getExportCSV: () => {
-    const config = {
-      method: 'GET',
-      url: `${process.env.backendUrl}ExportCSV.php`,
-      dataType: 'json',
-      contentType: 'application/json',
-    };
-    return axios(config);
-  },
+  // putHistory: () => {
+  //   const config = {
+  //     method: 'PUT',
+  //     url: `${process.env.backendUrl}history.php`,
+  //     dataType: 'json',
+  //     contentType: 'application/json',
+  //   };
+  //   return axios(config);
+  // },
 };
 
 export default backendService;
